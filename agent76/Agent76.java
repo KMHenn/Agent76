b6712835ffd46f01d510f8320d1f535b94b0a152
KMHenn <kaithennessy@gmail.com>
2017-06-29 11:32:21 +0900

    6/29 build before semi-reset

--- agent76/Agent76.java
+++ agent76/Agent76.java
@@ -1,19 +1,12 @@
 package agent76;
 
-import java.util.List;
-
+import java.util.ArrayList;
 import negotiator.Agent;
-import negotiator.AgentID;
 import negotiator.Bid;
-import negotiator.Deadline;
 import negotiator.actions.Accept;
 import negotiator.actions.Action;
 import negotiator.actions.Offer;
-import negotiator.parties.AbstractNegotiationParty;
-import negotiator.parties.NegotiationInfo;
-import negotiator.persistent.PersistentDataContainer;
 import negotiator.timeline.Timeline;
-import negotiator.utility.AbstractUtilitySpace;
 import negotiator.utility.UtilitySpace;
 
 /**
@@ -27,6 +20,19 @@ public class Agent76 extends Agent{
 	private UtilitySpace utilitySpace;
 	private Timeline timeline;
 	private Action lastOpponentAction = null;
+	private ArrayList <Offer> previousOpponentOffers;
+	private static double MINIMUM_UTILITY = 0.00;
+	
+	
+	/**
+	 * Informs the agent that a new negotiation session has begun.
+	 */
+	@Override
+	public void init(){
+		super.init();
+		//TODO
+		MINIMUM_UTILITY = utilitySpace.getReservationValue();
+	}
 	
 	/**
 	 * Return the action the agent chooses to make next.
@@ -36,25 +42,30 @@ public class Agent76 extends Agent{
 	@Override
 	public Action chooseAction() {
 		// TODO
-		Action newAction = null;
+		Action newAction = null; //TODO always null as of now
 		try{
 			if (lastOpponentAction == null) // First turn of first round, must bid.
 				//TODO make some kind of bid
+				return null;
+			
 			if (lastOpponentAction instanceof Offer){ // Action must be decided based on circumstances.
+				previousOpponentOffers.add((Offer) lastOpponentAction);
 				Bid opponentBid = ((Offer) lastOpponentAction).getBid();
 				double offeredOpponentUtil = getUtility(opponentBid);
+				
 				// get current time
 				double time = timeline.getTime();
+				
 				//TODO make some kind of bid
 				
 				Bid newBid = ((Offer) newAction).getBid();
 				double myOfferedUtil = getUtility(newBid);
 				
-				//accept under certain circumstances
-				if (isAcceptable(offeredOpponentUtil, myOfferedUtil, time))
+				if (isAcceptable(offeredOpponentUtil, myOfferedUtil, time)) // Check if offer should be accepted.
 					newAction = new Accept(getAgentID(), newBid);
 			}
-		} catch (Exception e){
+		}
+		catch (Exception e){
 			e.printStackTrace();
 			newAction = new Accept(getAgentID(), null); //TODO probably shouldn't be null.
 		}
@@ -69,15 +80,6 @@ public class Agent76 extends Agent{
 	@Override
 	public void ReceiveMessage(Action opponentAction){
 		lastOpponentAction = opponentAction;
-		//TODO
-	}
-	
-	/**
-	 * Informs the agent that a new negotiation session has begun.
-	 */
-	@Override
-	public void init(){
-		super.init();
 		//TODO
 	}
 	
@@ -103,19 +105,32 @@ public class Agent76 extends Agent{
 	 */
 	public boolean isAcceptable(double opponentOffer, double myOffer, double time){
 		//TODO
-		double probableAcceptance = ((opponentOffer - (2*opponentOffer*time)
+		double paccept = ((opponentOffer - (2*opponentOffer*time)
 				+ 2*(time - 1 + Math.sqrt((time - 1)*(time - 1) + opponentOffer*((2*time) - 1))))
 				/ ((2*time) - 1));
-		if (probableAcceptance >= 0.75)
+		if (paccept >= 0.75)
 			return true;
 		
 		return false;
 	}
 	
 	/**
+	 * Calculate the average utility of the opponent's past bids.
+	 * 
+	 * @return average opponent utility.
+	 */
+	private double averageOpponentUtility(){
+		double numerator = 0;
+		for (int i = 0; i < previousOpponentOffers.size(); i ++)
+			numerator += getUtility(previousOpponentOffers.get(i).getBid()); 
+		
+		return (numerator / previousOpponentOffers.size());
+	}
+	
+	/**
 	 * Return the name of the agent.
 	 * 
-	 * @return
+	 * @return name of the agent.
 	 */
 	@Override
 	public String getName(){
---
+++ agent76/GenerateBid.java
@@ -0,0 +1,31 @@
+package agent76;
+
+import negotiator.Bid;
+
+/**
+ * Class to handle deciding bids.
+ * 
+ * @author Kaitlyn
+ *
+ */
+public class GenerateBid {
+	static double maxBid = 0.00;
+	
+	public GenerateBid(){
+		//todo
+	}
+	
+	public Bid getBid(Bid base, double threshold){
+		//TODO
+		return null;
+	}
+	
+	public Bid getAppropriateBidBySearch(Bid base, double threshold){
+		//TODO
+		return null;
+	}
+	
+	private void setMaxBid(){
+		//TODO
+	}
+}
