baec7b600957e9c12629072e971d198c7b7c4262
Kaitlyn <kaithennessy@gmail.com>
2017-07-06 11:39:26 +0900

    Updated comments.

--- Agent76.java
+++ Agent76.java
@@ -20,15 +20,13 @@ import negotiator.parties.NegotiationInfo;
  *
  */
 public class Agent76 extends AbstractNegotiationParty{
-	private Bid lastPartnerBid;
-	static double MIN_BID_UTILITY;
-	private static double alpha = .30769;
-	private static double utilDec = .025;
-	
-	OpponentTracker opponent;
+	private Bid lastPartnerBid; // Track the last bid made by the opponent.
+	static double MIN_BID_UTILITY; // Hold the minimum bid utility.
+	private static double alpha = .30769; // Arbitrary alpha value used for calculating threshold.
 	
 	/**
 	 * Called at the beginning of negotiations.
+	 * Initiates all values needed for the negotiation.
 	 * 
 	 * @param info
 	 */
@@ -37,7 +35,6 @@ public class Agent76 extends AbstractNegotiationParty{
 		super.init(info);
 		MIN_BID_UTILITY = utilitySpace.getReservationValueUndiscounted();
 		lastPartnerBid = null;
-		opponent = new OpponentTracker();
 	}
 	
 	/**
@@ -47,18 +44,18 @@ public class Agent76 extends AbstractNegotiationParty{
 	 */
 	@Override
 	public Action chooseAction(List<Class<? extends Action>> validActs) {
-		Action action = null;
-		double time = timeline.getTime();
-		double threshold = getThreshold(time);
+		Action action = null; // To hold the chosen action.
+		double time = timeline.getTime(); // Get the current normalized time of the negotiation.
+		double threshold = getThreshold(time); // Calculate the threshold based on time.
 		
 		try{
-			if ((validActs.size() == 2) && (validActs.contains(Offer.class)))
+			if ((validActs.size() == 2) && (validActs.contains(Offer.class))) // Check if an initial offer needs to be made.  Size 2 because initial valid options are Offer and EndNegotiations.
 				action = new Offer(getPartyId(), generateBid(threshold));
 		
-			else if (validActs.contains(Accept.class)){
+			else if (validActs.contains(Accept.class)){ // Decide how to behave based on previous bids.
 				double currentOppUtility = getUtilitySpace().getUtility(lastPartnerBid);
 			
-				if (isFair(currentOppUtility, (1 - currentOppUtility)))
+				if (isFair(currentOppUtility, (1 - currentOppUtility))) // Check if the bid is fair for Agent76.
 					action = new Accept(getPartyId(), lastPartnerBid);
 			
 				else if (threshold >= MIN_BID_UTILITY){ // Bids are still within what we've determined to be a valid range.
@@ -78,14 +75,6 @@ public class Agent76 extends AbstractNegotiationParty{
 	}
 	
 	/**
-	 * Use Jain's fairness index to decide whether or not the bid
-	 * is fair for Agent76.
-	 * 
-	 * @return true if fair, false otherwise.
-	 */
-
-	
-	/**
 	 * Receive the opponent's action as a message.
 	 * 
 	 * @param sender
@@ -95,8 +84,8 @@ public class Agent76 extends AbstractNegotiationParty{
 	public void receiveMessage(AgentID sender, Action action){
 		super.receiveMessage(sender, action);
 		
-		if(action instanceof Offer){
-			lastPartnerBid = ((Offer) action).getBid(); //TODO problem here?
+		if(action instanceof Offer){ // Check if the opponent just made an offer.
+			lastPartnerBid = ((Offer) action).getBid();
 		}
 	}
 	
@@ -108,18 +97,12 @@ public class Agent76 extends AbstractNegotiationParty{
 	 * @throws Exception 
 	 */
 	private Bid generateBid(double threshold) throws Exception{
-		//TODO want to prevent just handing the opponent the win with a bad bid.
-		Bid bid = utilitySpace.getMinUtilityBid();
-		double desiredUtility = getUtility(utilitySpace.getMaxUtilityBid());
+		Bid bid = utilitySpace.getMinUtilityBid(); // Start the bid using the minimum utility.
 		
-		while(!isFair(getUtility(bid), (1 - getUtility(bid)))){
+		while(!isFair(getUtility(bid), (1 - getUtility(bid)))) // Check if this bid is fair for Agent76.
 			bid = generateRandomBid();
-			desiredUtility -= utilDec;
-		}
 
-		utilDec += .025;
-		System.out.println("-----\nMy Util: " + getUtility(bid) + "\n-----");
-		
+		System.out.println("-----\nMy Util: " + getUtility(bid) + "\n-----"); // For debug.
 		return bid;
 	}
 	
@@ -128,7 +111,7 @@ public class Agent76 extends AbstractNegotiationParty{
 	 * the negotiation round.
 	 * 
 	 * @param time
-	 * @return
+	 * @return threshold, based on current time.
 	 */
 	public double getThreshold(double time){
 		return (1.0 - Math.pow(time,  (1/alpha)));
@@ -142,9 +125,9 @@ public class Agent76 extends AbstractNegotiationParty{
 	 * @return true if fair, false otherwise
 	 */
 	public boolean isFair(double myUtil, double oppUtil){
-		double comparison = myUtil / oppUtil;
+		double comparison = myUtil / oppUtil; // Ratio of Agent76's utility to the opponents.
 		
-		if (comparison >= 1)
+		if (comparison >= 1) // Utility split is at least even, if not better for Agent76.
 			return true;
 		
 		return false;
